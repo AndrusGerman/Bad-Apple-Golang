87c24db60fe2494e2fff39d25826c14f25ace29e
AndrusGerman <andrusdiazaleman@gmail.com>
2022-06-05 06:35:16 -0400

    bad apple easy for example

---
+++ app.go
@@ -0,0 +1,42 @@
+package main
+
+import (
+	"fmt"
+	"io/ioutil"
+	"os"
+	"os/exec"
+	"runtime"
+	"time"
+)
+
+func main() {
+	for frame := 1; frame <= 5966; frame++ {
+		clear()           // solo limpia la pantalla
+		printFrame(frame) // que frame imprimir
+		time.Sleep(29 * time.Millisecond)
+	}
+}
+
+func printFrame(frame int) {
+	var fileName = fmt.Sprintf("res/BA%d.txt", frame) // con el frame crea una variable del nombre del archivo
+	var frameContent = getFrame(fileName)             // gurda el contenido del frame
+	fmt.Fprint(os.Stdout, frameContent)               // imprime en pantalla el frame
+}
+
+func getFrame(frameDir string) string {
+	fileRaw, _ := ioutil.ReadFile(frameDir) // lee el archivo
+	return string(fileRaw)
+}
+
+func clear() {
+	if runtime.GOOS == "linux" {
+		cmd := exec.Command("clear")
+		cmd.Stdout = os.Stdout
+		cmd.Run()
+	}
+	if runtime.GOOS == "windos" {
+		cmd := exec.Command("cmd", "/c", "cls")
+		cmd.Stdout = os.Stdout
+		cmd.Run()
+	}
+}
---
+++ go.mod
@@ -0,0 +1,3 @@
+module badapple
+
+go 1.18
